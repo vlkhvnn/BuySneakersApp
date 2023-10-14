0310e7e33b1ac704ed4ef6772431bb9c60e71c2c
Alikhan Tangirbergen <tanir.alikh@gmail.com>
2023-10-15 03:26:23 +0600

    Catalog VC and shoeCollectionViewCell

---
+++ BuySneakers/BuySneakers/Views/ShoeCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ShoeCollectionViewCell.swift
+//  BuySneakers
+//
+//  Created by Alikhan Tangirbergen on 15.10.2023.
+//
+
+import UIKit
+
+class ShoeCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ BuySneakers/BuySneakers/Views/ShoeTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ShoeTableViewCell.swift
+//  BuySneakers
+//
+//  Created by Alikhan Tangirbergen on 15.10.2023.
+//
+
+import UIKit
+
+class ShoeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
