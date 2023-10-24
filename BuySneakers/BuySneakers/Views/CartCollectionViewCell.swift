f708c387d23797a70a3461c7b54cf1525a322e7a
Alikhan Tangirbergen <tanir.alikh@gmail.com>
2023-10-24 23:55:59 +0600

    CollectionViewCell of Cart and CartVC

---
+++ BuySneakers/BuySneakers/Views/CartCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CartCollectionViewCell.swift
+//  BuySneakers
+//
+//  Created by Alikhan Tangirbergen on 22.10.2023.
+//
+
+import UIKit
+
+class CartCollectionViewCell: UICollectionViewCell {
+    
+}
