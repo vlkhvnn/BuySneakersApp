86ea28a7677219678cbe4d4d82c2446102c63da2
Alikhan Tangirbergen <tanir.alikh@gmail.com>
2023-10-25 18:01:56 +0600

    Ebashil koroche ves' den'

---
+++ BuySneakers/BuySneakers/Models/Purchase.swift
@@ -0,0 +1,8 @@
+//
+//  Purchase.swift
+//  BuySneakers
+//
+//  Created by Alikhan Tangirbergen on 25.10.2023.
+//
+
+import Foundation
---
+++ BuySneakers/BuySneakers/Views/CartCellButtonView.swift
@@ -0,0 +1,20 @@
+//
+//  CartCellButtonView.swift
+//  BuySneakers
+//
+//  Created by Alikhan Tangirbergen on 25.10.2023.
+//
+
+import UIKit
+
+class CartCellButtonView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
