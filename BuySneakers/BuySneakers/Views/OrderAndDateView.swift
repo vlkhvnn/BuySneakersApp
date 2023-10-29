ac3d202d85f540bfbae23ef9367f48401453e44c
Alikhan Tangirbergen <tanir.alikh@gmail.com>
2023-10-29 13:23:33 +0600

    OrderHistory VC completed!!!

---
+++ BuySneakers/BuySneakers/Views/OrderAndDateView.swift
@@ -0,0 +1,20 @@
+//
+//  OrderAndDateView.swift
+//  BuySneakers
+//
+//  Created by Alikhan Tangirbergen on 29.10.2023.
+//
+
+import UIKit
+
+class OrderAndDateView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
