b970444f185f19920b985cd0539e856835790713
Alikhan Tangirbergen <tanir.alikh@gmail.com>
2023-10-31 00:01:30 +0600

    ShoeSizeVC saveChanges functionality and saving user info in authentication process

---
+++ BuySneakers/BuySneakers/Views/Order/OrderTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  OrderTableViewCell.swift
+//  BuySneakers
+//
+//  Created by Alikhan Tangirbergen on 30.10.2023.
+//
+
+import UIKit
+
+class OrderTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
